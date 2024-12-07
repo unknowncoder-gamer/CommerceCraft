4b59f321eb500abc7835278050d36a87c23e6eaf
unknowncoder-gamer <rajithaanu1985@gmail.com>
2024-12-07 20:58:54 +0530

    Auth setup + convex auth

---
+++ config/siteConfig.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "CommerceCraft",
+  description:
+    "CommerceCraft is an e-commerce service where users can make create store and manage analytics, get individual product information, etc. It is also available for even a small startups with small amount for an year and start free trail for over 30days.",
+};
